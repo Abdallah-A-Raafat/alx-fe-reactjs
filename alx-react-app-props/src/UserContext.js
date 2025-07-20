2b9ca116714492ab53f9eb7ced0f96acb6ced132
abdallah <abdallah.araafat@gmail.com>
2025-07-20 23:51:57 +0300

    enhancing styles

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,6 +1,6 @@
 import { createContext } from 'react';
 
-// Create the UserContext
-const UserContext = createContext();
+// Create the UserContext with undefined as default value
+const UserContext = createContext(undefined);
 
 export default UserContext;
